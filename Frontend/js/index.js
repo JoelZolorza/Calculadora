6a2400a39f16d1766d04504dd477c5bf479493e4
joel <joelzolorza65@gmail.com>
2023-01-21 23:49:32 -0300

    Update

--- Frontend/js/index.js
+++ Frontend/js/index.js
@@ -6,6 +6,11 @@ const botonesOperadores = document.querySelectorAll('.operador');
 const display = new Display(displayValorAnterior, displayValorActual);
 
 botonesNumeros.forEach(boton => {
-    boton.addEventListener('click',() => display.agregarNumero(boton.innerHTML));
+    boton.addEventListener('click',() => display.agregarNumero(boton.innerHTML))
 });
 
+
+
+botonesOperadores.forEach(boton => {
+    boton.addEventListener('click', () => display.computar(boton.value))
+});
